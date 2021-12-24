eaa2cd86037afa9a323dd15d135155b2d9e37fbf
misha-wit <20095904@mail.wit.ie>
2021-12-24 19:33:51 +0530

    Added Vue for Activities

---
+++ src/main/resources/vue/views/activity-profile.vue
@@ -0,0 +1,116 @@
+<template id="activity-profile">
+  <app-layout>
+    <div v-if="noActivityFound">
+      <p> We're sorry, we were not able to retrieve this activity.</p>
+      <p> View <a :href="'/activities'">all activities</a>.</p>
+    </div>
+    <div class="card bg-light mb-3" v-if="!noActivityFound">
+      <div class="card-header">
+        <div class="row">
+          <div class="col-6"> Activity Profile </div>
+          <div class="col" align="right">
+            <button rel="tooltip" title="Update"
+                    class="btn btn-info btn-simple btn-link"
+                    @click="updateActivity()">
+              <i class="far fa-save" aria-hidden="true"></i>
+            </button>
+            <button rel="tooltip" title="Delete"
+                    class="btn btn-info btn-simple btn-link"
+                    @click="deleteActivity()">
+              <i class="fas fa-trash" aria-hidden="true"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="card-body">
+        <form>
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="input-activity-id">Activity ID</span>
+            </div>
+            <input type="number" class="form-control" v-model="activity.id" description="id" readonly placeholder="Id"/>
+          </div>
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="input-activity-description">Description</span>
+            </div>
+            <input type="text" class="form-control" v-model="activity.description" description="description" placeholder="Description"/>
+          </div>
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="input-activity-description">Description</span>
+            </div>
+            <input type="description" class="form-control" v-model="activity.description" description="description" placeholder="Description"/>
+          </div>
+        </form>
+      </div>
+      <div class="card-footer text-left">
+        <p  v-if="activities.length == 0"> No activities yet...</p>
+        <p  v-if="activities.length > 0"> Activities so far...</p>
+        <ul>
+          <li v-for="activity in activities">
+            {{ activity.description }} for {{ activity.duration }} minutes
+          </li>
+        </ul>
+      </div>
+    </div>
+  </app-layout>
+</template>
+
+<script>
+Vue.component("activity-profile", {
+  template: "#activity-profile",
+  data: () => ({
+    activity: null,
+    noActivityFound: false,
+    activities: [],
+  }),
+  created: function () {
+    const activityId = this.$javalin.pathParams["activity-id"];
+    const url = `/api/activities/${activityId}`
+    axios.get(url)
+        .then(res => this.activity = res.data)
+        .catch(error => {
+          console.log("No activity found for id passed in the path parameter: " + error)
+          this.noActivityFound = true
+        })
+    axios.get(url + `/activities`)
+        .then(res => this.activities = res.data)
+        .catch(error => {
+          console.log("No activities added yet (this is ok): " + error)
+        })
+  },
+  methods: {
+    updateActivity: function () {
+      const activityId = this.$javalin.pathParams["activity-id"];
+      const url = `/api/activities/${activityId}`
+      axios.patch(url,
+          {
+            description: this.activity.description,
+            description: this.activity.description
+          })
+          .then(response =>
+              this.activity.push(response.data))
+          .catch(error => {
+            console.log(error)
+          })
+      alert("Activity updated!")
+    },
+    deleteActivity: function () {
+      if (confirm("Do you really want to delete?")) {
+        const activityId = this.$javalin.pathParams["activity-id"];
+        const url = `/api/activities/${activityId}`
+        axios.delete(url)
+            .then(response => {
+              alert("Activity deleted")
+              //display the /activities endpoint
+              window.location.href = '/activities';
+            })
+            .catch(function (error) {
+              console.log(error)
+            });
+      }
+    }
+  }
+});
+</script>
